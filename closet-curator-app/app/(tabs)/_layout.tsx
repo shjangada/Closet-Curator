09b26ff0fe072ec492a5aa85be34992d2e91922d
shjangada <jangada2@illinois.edu>
2024-10-18 14:34:07 -0500

    updated the welcome screen

--- closet-curator-app/app/(tabs)/_layout.tsx
+++ closet-curator-app/app/(tabs)/_layout.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useRouter } from 'expo-router';
+
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
--- closet-curator-app/app/(tabs)/welcome.tsx
+++ closet-curator-app/app/(tabs)/welcome.tsx
@@ -1,29 +1,74 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, View, Text, TouchableOpacity } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useRouter } from 'expo-router'; // Import useRouter
+
+
 
 export default function WelcomeScreen() {
+  const router = useRouter(); 
+
   return (
-    <ThemedText>
-        Welcome Screen
-    </ThemedText>
+    <ThemedView style={styles.container}>
+      <ThemedText style={styles.welcomeText}>Welcome to Closet Curator</ThemedText>
+      <Image
+          source={require('@/assets/images/Closet-Curator-Logo.png')}
+          style={styles.logo}
+        resizeMode="contain"
+      />
+      <TouchableOpacity style={styles.loginButton} onPress={() => router.push('/login')}>
+        <ThemedText style={styles.buttonText}>Log In</ThemedText>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.signupButton}>
+        <ThemedText style={styles.buttonText}>Sign Up</ThemedText>
+      </TouchableOpacity>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0c0b32',
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  welcomeText: {
+    color: 'white',
+    fontSize: 50,
+  },
+  appName: {
+    color: 'white',
+    fontSize: 28,
+    marginVertical: 10,
+  },
+  logo: {
+    width: '20%', 
+    marginVertical: 20,
+  },
+  loginButton: {
+    width: 400,
+    padding: 20,
+    margin: 10,
+    backgroundColor: '#a292be', 
+    borderRadius: 20,
+    alignItems: 'center',
+  },
+  signupButton: {
+    width: 400,
+    padding: 20,
+    margin: 10,
+    backgroundColor: '#a292be', 
+    borderRadius: 20,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 30,
   },
 });
